c05fd7f078a8c9243dcaa8f9efc8cd82d414bf9a
ShabnamGoyal <82241004+ShabnamGoyal@users.noreply.github.com>
2021-05-21 19:47:19 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/* Declare three variables, a and b and c.*/
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = 'let a = 3; \nlet b = 5; \nlet c;\n-------------';
+
+output = output + '\na+b =' + (a + b);
+output = output + '\na-b =' + (a - b);
+output = output + '\na*b =' + (a * b);
+output = output + '\na/b =' + (a / b);
+output = output + '\na%b =' + (a % b);
+output = output + '\na+=b =' + (a += b);
+output = output + '\na-=b =' + (a -= b);
+output = output + '\na*=b =' + (a *= b);
+output = output + '\na/=b =' + (a /= b);
+output = output + '\na%=b =' + (a %= b);
+output = output + '\na==b =' + (a == b);
+output = output + '\na!=b =' + (a != b);
+output = output + '\na>b =' + (a > b);
+output = output + '\na<b =' + (a < b);
+output = output + '\n!a && !c =' + (!a && !c);
+output = output + '\n!a || !c =' + (!a || !c);
+alert(output);
