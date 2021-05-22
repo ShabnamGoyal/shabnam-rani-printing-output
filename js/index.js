7094a3c4767c67fef9afe230f0b381ebc03de305
ShabnamGoyal <82241004+ShabnamGoyal@users.noreply.github.com>
2021-05-21 20:08:43 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,3 +23,11 @@ output = output + '\na<b =' + (a < b);
 output = output + '\n!a && !c =' + (!a && !c);
 output = output + '\n!a || !c =' + (!a || !c);
 alert(output);
+
+let first_name = 'Shabnam';
+let last_name = 'Goyal';
+let email = 'rani0035@algonquinlive.com';
+
+let result = '\nMy name is ' + first_name + ' ' + last_name +
+    '.\nYou can contact me at ' + email + '.';
+alert(result);
